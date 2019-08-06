751477769d64662daa6827a3e796319a6d712146
Ronit Shrivastava <53787875+ronit398@users.noreply.github.com>
2019-08-06 11:19:42 +0530

    Add files via upload

---
+++ SubArrayWithGivenSum/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int T;
+    cout<<"Enter the number of test case: ";
+    cin>>T;
+    while(T--)
+    {
+        int N,i;
+        cout<<"Enter the size of array: ";
+        cin>>N;
+        int arr[N];
+        cout<<"Enter the elements in array: ";
+        for(i=0,i<N;i++)
+
+    }
+    return 0;
+}
